f43d25e1a49fde64467ce1b3cdaac87f72dd7680
Fábio Antunes <fabioantuness@gmail.com>
2015-06-09 22:42:59 +0100

    Added range input sharing expense with friends

--- app/scripts/controllers/share.modal.controller.js
+++ app/scripts/controllers/share.modal.controller.js
@@ -7,7 +7,11 @@ angular.module('controllers')
     search: ''
     
   };
-  
+  $scope.me = {
+    expenseValue: $scope.expense.value
+  }
+  $scope.equal = true;
+
   $ionicPopover.fromTemplateUrl('templates/popover-search-friend.html', {
     scope: $scope
   }).then(function(popover) {
@@ -15,16 +19,37 @@ angular.module('controllers')
   });
 
   $scope.addFriend = function(index){
-    $scope.shareModal.shareFriends.push($scope.shareModal.friends.splice(index, 1)[0]);
+    var friend = $scope.shareModal.friends.splice(index, 1)[0];
+    friend.expenseValue = 0;
+    $scope.shareModal.shareFriends.push(friend);
+    $scope.recalcTotal();
     $scope.popover.hide();
+    $scope.shareModal.search = '';
   };
+
   $scope.triggerPopover = function($event){
-    if($scope.shareModal.search.search.length && !$scope.popover.isShown()){
+    if($scope.shareModal.search && $scope.shareModal.search.length && !$scope.popover.isShown()){
       $scope.popover.show($event);
 
-    }else if($scope.shareModal.search.search.length<1 && $scope.popover.isShown()){
+    }else if(!$scope.shareModal.search && $scope.popover.isShown()){
       $scope.popover.hide();
     }
-  }
+  };
+
+  $scope.recalcTotal = function(){
+    var i = 0;
+    var length = $scope.shareModal.shareFriends.length;
+    
+    if($scope.equal){
+
+      var equalExpense = $scope.expense.value / (length + 1);
+
+      for(i = 0; i < length; i++){
+        $scope.shareModal.shareFriends[i].expenseValue = equalExpense;
+      }
+      $scope.me.expenseValue = equalExpense;
+      
+    }
+  };
 
 }]);
